7c90c42ef9acf3602be4b400df70b43a52003315
johnmart19 <johnivan19999@gmail.com>
2023-02-26 12:48:21 +0200

    munch: Import Xiaomi TouchFeature service
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,6 +166,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
