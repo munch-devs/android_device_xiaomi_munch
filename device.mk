cef1baa622b66880edc06c1e79d634247306a7c0
HDzungx <hdzungx@gmail.com>
2024-09-26 11:13:30 +0700

    munch: Build vendor.xiaomi.hardware.touchfeature@1.0 from source
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Touchfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
