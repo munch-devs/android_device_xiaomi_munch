c23d556b129cde7b7be4953e2f9adf13eb22bdca
HDzungx <hdzungx@gmail.com>
2025-07-03 18:39:13 +0700

    munch: @ProjectInfinity-X
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- infinity_munch.mk
+++ infinity_munch.mk
@@ -8,17 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := infinity_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22021211RC
+
+INFINITY_MAINTAINER := "HDzungx"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
