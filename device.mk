4596fb9498fc0b1542330da66de53144653e34ea
HDzungx <hdzungx@gmail.com>
2026-02-11 22:25:33 +0700

    munch: @HertzifyOS
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- device.mk
+++ device.mk
@@ -356,7 +356,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel/pixelstats \
     hardware/google/pixel/power-libperfmgr \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/hertzify/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
