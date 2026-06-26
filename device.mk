47298792638ee3d391ebf4705087de4adc3d4421
Bruno Martins <bgcngm@gmail.com>
2026-06-27 00:03:32 +0100

    munch: Remove disable_configstore from PRODUCT_PACKAGES
    Configstore is dead in CP2A.
    
    Change-Id: I8efc0e2171c2841fa888d8b11b33691f955bd36d
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- device.mk
+++ device.mk
@@ -173,10 +173,6 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_munch)
 
-# Configstore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.lineage
