f00ad890ee7edd427b6a6ffd7074fc90e16dbe33
HDzungx <hdzungx@gmail.com>
2026-08-08 09:08:19 +0700

    Revert "munch: Update kernel defconfig for E404"
    This reverts commit 607166054095887a5a1bd6a2add023e68d932e7e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,11 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/munch
-TARGET_KERNEL_CONFIG := vendor/munch_defconfig
+TARGET_KERNEL_CONFIG := \
+    vendor/kona-perf_defconfig \
+    vendor/debugfs.config \
+    vendor/xiaomi/sm8250-common.config \
+	vendor/xiaomi/munch.config
 
 # Miui Camera
 TARGET_INCLUDES_MIUI_CAMERA := true
